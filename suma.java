e0c1345a76c6052899cd4d8cedffa1052930f44e
santiago <acostasntiago@gmail.com>
2026-03-05 18:43:41 -0500

    Se agrega la operacion de suma

---
+++ suma.java
@@ -0,0 +1,11 @@
+public class Suma {
+
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int resultado = sumar(5, 3);
+        System.out.println("La suma es: " + resultado);
+    }
+}
